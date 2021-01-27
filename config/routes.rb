92e47ef965b7154a6f18a4308efbdc945b79407d
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-01-27 12:34:08 -0600

    generate sessions controller; fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  get 'users/show'
+  get 'login' => 'sessions/new'
+  post 'login' => 'sessions/create' 
+  post 'logout' => 'sessions#destroy'
+
+  get 'signup' => 'users#new'
+
+  
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
