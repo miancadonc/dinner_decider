2790cff24c801c67c27e2a31423b90d5753e3595
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-01-27 12:34:36 -0600

    routes syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'login' => 'sessions/new'
-  post 'login' => 'sessions/create' 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create' 
   post 'logout' => 'sessions#destroy'
 
   get 'signup' => 'users#new'
