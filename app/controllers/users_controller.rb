bf953dc51983f6fe2b46b6e41fcdab48de2e48cb
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-01-27 16:20:17 -0600

    create user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 
   def edit
@@ -15,5 +19,13 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+
 end
