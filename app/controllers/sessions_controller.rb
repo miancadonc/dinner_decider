e6ee5e781da8771b7428457055c6866e6414c46e
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-02 15:55:20 -0600

    create failed log in error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,12 @@ class SessionsController < ApplicationController
   def create
 
     @user = User.find_by(name: params[:name])
-    return head(:forbidden) unless @user.try(:authenticate, params[:password])
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
+    if @user.try(:authenticate, params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to login_path, alert: "Unable to log in, please check username and password"
+    end
 
   end
 
