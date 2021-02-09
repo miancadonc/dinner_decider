5a536f0605aad777836310d8893d81b4bae3e933
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-09 14:38:40 -0600

    refactor views for email changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true
-    validates :name, uniqueness: { message: "Username already taken" }
+    validates :email, uniqueness: { message: "Email already taken" }
     
 end
