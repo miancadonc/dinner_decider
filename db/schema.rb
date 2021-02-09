490ff2e430a94e65b4f6ed690169e655d7582482
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-09 14:35:54 -0600

    add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_220942) do
+ActiveRecord::Schema.define(version: 2021_02_09_201223) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
+    t.string "email"
   end
 
 end
