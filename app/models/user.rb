6aef5b84523858b8d7bf9356ce8d9e4b204c4141
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-10 20:48:55 -0600

    def User.create_from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true
-    validates :email, uniqueness: { message: "Email already taken" }
+    # validates :email, uniqueness: { message: "Email already taken" }
+
+    def self.create_from_omniauth(auth_hash)
+        
+        @user = User.create do |user|
+            user.name = auth_hash[:info][:name]
+            user.email = auth_hash[:info][:email]
+            user.uid = auth_hash[:uid]
+            user.provider = auth_hash[:provider]
+            user.password = auth_hash[:credentials][:token]
+        end
+
+    end
     
 end
