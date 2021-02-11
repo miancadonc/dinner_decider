abd8f319b32933a24b7e4f2728617d6e54c48675
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-10 20:49:41 -0600

    add helpers for controllers to log in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+
+    def sign_in_with_auth(auth_hash)
+
+        #use uid to find; assign provider & password
+        @user = User.find_by(uid: auth_hash[:uid])
+
+        if @user
+            session[:user_id] = @user.id
+        else
+            @user = User.create_from_omniauth(auth_hash)
+        end
+        redirect_to user_path(@user)
+    end
+
+    def sign_in_with_password
+
+        @user = User.find_by(email: params[:email])
+
+        #normal bcrypt authentication, plus verify that oauth provider isn't present
+        if @user.try(:authenticate, params[:password]) && @user.provider == nil
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to login_path, alert: "Unable to log in, please check email and password"
+        end
+
+    end
+
+
 end
