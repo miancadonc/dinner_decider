41fc063c14507e4007c060c082b9da4a89d8b8da
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-13 13:44:35 -0600

    generate recipe_ingredients, limit routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
     resources :recipes
     resources :ingredients
   end
+
+  resources :recipe_ingredients, only: [:create, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_190205) do
+ActiveRecord::Schema.define(version: 2021_02_13_191342) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.string "unit"
+    t.float "magnitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
