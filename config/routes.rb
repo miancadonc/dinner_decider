ebaeb6563d434064b4d5356bb441ac129dff9274
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-13 13:10:37 -0600

    generate ingredients; nest routes in users for now

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   get 'signup' => 'users#new'
 
-  
+
   resources :users do
     resources :recipes
+    resources :ingredients
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
