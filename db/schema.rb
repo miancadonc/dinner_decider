16aa2cf176dbdbc32e9aaa7ec2f6af82175e7ea8
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-13 15:02:28 -0600

    add references to recipe ingredient join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_191342) do
+ActiveRecord::Schema.define(version: 2021_02_13_204915) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2021_02_13_191342) do
     t.float "magnitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "recipe_id", null: false
+    t.integer "ingredient_id", null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -45,4 +49,6 @@ ActiveRecord::Schema.define(version: 2021_02_13_191342) do
     t.string "provider"
   end
 
+  add_foreign_key "recipe_ingredients", "ingredients"
+  add_foreign_key "recipe_ingredients", "recipes"
 end
