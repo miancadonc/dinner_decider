d34b6cb15d642b16e99541f0f49844430677a480
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-23 15:59:39 -0600

    build out dummy nested form for testing, add nested attributes for to user model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,13 @@
 class RecipesController < ApplicationController
 
     def show
-        @recipe = Recipe.find_by(id: params[:id])
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def new
+        @user = User.find(params[:user_id])
+        @recipe = @user.recipes.build
+        15.times { @recipe.recipe_ingredients.build }
     end
 
 end
