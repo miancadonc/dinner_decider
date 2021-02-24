64d2016ee6d1175182d7c6983b54f9c9e2c91957
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-24 14:31:46 -0600

    abstract out ingredient sort from view

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,10 @@ class Ingredient < ApplicationRecord
 
     has_many :recipe_ingredients
     has_many :recipes, through: :recipe_ingredients
+
+    def self.list_alphabetically
+        self.all.sort{|a,b| a.name <=> b.name}
+    end
+
+
 end
