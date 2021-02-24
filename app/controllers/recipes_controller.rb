22c2ad296cae17c1bc2f84055eededce8825b1c1
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-02-24 15:18:19 -0600

    create new and create actions for recipes - hard coded to number of ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,4 +10,22 @@ class RecipesController < ApplicationController
         15.times { @recipe.recipe_ingredients.build }
     end
 
+    def create
+        @user = User.find(params[:user_id])
+        recipe = @user.recipes.build(recipe_params)
+
+        if recipe.save
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
+
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :description, :instructions, recipe_ingredients_attributes: [:ingredient_id, :magnitude, :unit] )
+    end
+
 end
