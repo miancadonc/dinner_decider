0082b1e1b9626728adb4958a8aeb9e04cb5111b7
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-01 15:20:44 -0600

    seed database with tags for tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-recipes = Recipe.create([{name: "Udon", user_id: 5}, {name: "Grilled Cheese", user_id: 5}, {name: "Gumbo", user_id: 5}])
-ingredients = Ingredient.create(
-    [{name: "onion"}, {name: "carrot"}, {name: "ginger"}, {name: "potato"}, {name: "soy sauce"}, {name: "egg"}, {name: "milk"}, {name: "sriracha" }, {name: "mustard" },
-        {name: "yellow onion" }, {name: "red onion" }, {name: "shallot" }, {name: "green bell pepper" }, {name: "red bell pepper" }, {name: "flour" }, {name: "yeast" },
-        {name: "salt" }, {name: "sugar" }, {name: "turbinado sugar" }, {name: "water" }, {name: "vinegar" }, {name: "balsamic vinegar" }, {name: "bay leaf" },
-        {name: "cayenne" }, {name: "curry powder" }, {name: "curry paste" }, {name: "jalapeno pepper" }, {name: "cilantro" }, {name: "cumin" }, {name: "paprika" },
-        {name: "bread crumbs" }, {name: "lemon zest" }, {name: "lemon juice" }, {name: "orange zest" }, {name: "orange juice" }, {name: "chocolate chip" }, {name: "olive oil" },
-        {name: "butter" }, {name: "unsalted butter" }, {name: "spinach" }, {name: "peanut butter" }, {name: "american cheese" }, {name: "cheese" }, {name: "cheddar" },
-        {name: "parmesan" }, {name: "asiago" }, {name: "bacon" }, {name: "ground beef" }, {name: "chicken thigh" }, {name: "rosemary" }, {name: "mushroom" },]
-)
+# recipes = Recipe.create([{name: "Udon", user_id: 5}, {name: "Grilled Cheese", user_id: 5}, {name: "Gumbo", user_id: 5}])
+# ingredients = Ingredient.create(
+#     [{name: "onion"}, {name: "carrot"}, {name: "ginger"}, {name: "potato"}, {name: "soy sauce"}, {name: "egg"}, {name: "milk"}, {name: "sriracha" }, {name: "mustard" },
+#         {name: "yellow onion" }, {name: "red onion" }, {name: "shallot" }, {name: "green bell pepper" }, {name: "red bell pepper" }, {name: "flour" }, {name: "yeast" },
+#         {name: "salt" }, {name: "sugar" }, {name: "turbinado sugar" }, {name: "water" }, {name: "vinegar" }, {name: "balsamic vinegar" }, {name: "bay leaf" },
+#         {name: "cayenne" }, {name: "curry powder" }, {name: "curry paste" }, {name: "jalapeno pepper" }, {name: "cilantro" }, {name: "cumin" }, {name: "paprika" },
+#         {name: "bread crumbs" }, {name: "lemon zest" }, {name: "lemon juice" }, {name: "orange zest" }, {name: "orange juice" }, {name: "chocolate chip" }, {name: "olive oil" },
+#         {name: "butter" }, {name: "unsalted butter" }, {name: "spinach" }, {name: "peanut butter" }, {name: "american cheese" }, {name: "cheese" }, {name: "cheddar" },
+#         {name: "parmesan" }, {name: "asiago" }, {name: "bacon" }, {name: "ground beef" }, {name: "chicken thigh" }, {name: "rosemary" }, {name: "mushroom" },]
+# )
+# tags = Tag.create(
+#     [
+#         {name: "Curry"}, {name: "Stew"}, {name: "Vegetarian"}, {name: "Vegan"}, {name: "Pescatarian"}, {name: "Baking"}, {name: "Healthy"}, {name: "Spicy"}, {name: "Very Spicy"}, {name: "American"},
+#         {name: "French"}, {name: "Thai"}, {name: "British"}, {name: "Chinese"}, {name: "Vietnamese"}, {name: "Mexican"}, {name: "Indian"}, {name: "Syrian"}, {name: "Turkish"}, {name: "Sandwich"},
+#         {name: "Soup"}, {name: "Salad"}, {name: "Dessert"}, {name: "Pastry"}, {name: "Cupcakes"}, {name: "Cake"}, {name: "Favorite"}, {name: "Casserole"}
+#     ]
+# )
+
+# If you need to seed more data, comment it out after
