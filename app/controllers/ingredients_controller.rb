301eeb02f768d4acc3508c6ac0c9093793799e6f
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-05 16:36:00 -0600

    correct comment typo

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -6,6 +6,6 @@ class IngredientsController < ApplicationController
     end
 
     # this controller method and its partner in the tag controller smell like code. 
-    # Maybe introducing before actions to the controllers (all of them, even) for params serchable objects is needed.
+    # Maybe introducing before actions to the controllers (all of them, even) for params searchable objects is needed.
 
 end
