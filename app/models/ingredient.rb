53fe00653f1536a2a99f53f4db7ee81d22152442
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-08 17:23:29 -0600

    allow for dependent destroy on tags and ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
 
-    has_many :recipe_ingredients
+    has_many :recipe_ingredients, :dependent => :destroy
     has_many :recipes, through: :recipe_ingredients
 
     
