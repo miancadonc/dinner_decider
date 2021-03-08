f594e129b0a07807fa05894711836a4b93acc3e9
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-08 17:29:59 -0600

    build out create, new, index, and destroy actions for tags_controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,4 +5,38 @@ class TagsController < ApplicationController
         @tag = Tag.find(params[:id])
     end
 
+    def new
+        @tag = Tag.new
+    end
+
+    def create
+        @tag = Tag.new(tag_params)
+
+        if @tag.save
+            flash[:notice] = "Tag successfully created"
+            redirect_to new_tag_path
+        else
+            @tag.name = nil
+            flash[:alert] = "Something went wrong, tag not saved"
+            render 'new'
+        end
+    end
+
+    def index
+        @tags = Tag.all
+    end
+
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        flash[:notice] = "Tag deleted"
+        redirect_to tags_path
+    end
+
+    private
+
+    def tag_params
+        params.require(:tag).permit(:name)
+    end
+
 end
