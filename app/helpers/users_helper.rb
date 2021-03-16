f020f4c3ce989d6531afdd7bd15bead564bdb389
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-15 19:02:58 -0500

    refactor users_helper to user list_alphabetically method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,18 +4,17 @@ module UsersHelper
         user.recipes.sample(number)
     end
 
-    # user_ingredients(user) is basically a rehash of the no_blank_ingredients method in recipe_ingredients_helper.rb that takes in a different collection
-    # source of ingredients - maybe they can be combined somewhere
-
     def user_ingredients(user)
-        ingredients = user.ingredients.uniq.select {|ing| ing.name != ""  }.sort{|a, b| a.name <=> b.name }
+        ingredients = user.ingredients.list_alphabetically.uniq.drop(1)
+        
     end
 
     def user_tags(user)
-        tags = user.tags.uniq.sort{|a, b| a.name <=> b.name}
+        tags = user.tags.list_alphabetically.uniq
     end
 
-    # The final sort on user_ingredients and user_tags can probably be abstracted at some point, but the abstract sort I have now is a class level method on application_record
+    # user_tags and user_ingredients use the list_alphabetically method defined in application_record.rb
+    # using drop(1) in user_ingredients removes the blank ingredient but still returns the array, unlike .shift
 
     def user_recipes(user, object)
         object.recipes.select{|r| r.user == user }
